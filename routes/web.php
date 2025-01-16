a4d4ee051941254a962694ffa3a4b9011d858ff8
Itamar Junior <cdajuniorf@gmail.com>
2025-01-16 18:28:34 -0300

    feat: Criar o controller para autenticação.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
+// auth routes
+Route::get('/login', action: [AuthController::class, 'login']);
+Route::get('/logout', action: [AuthController::class, 'logout']);

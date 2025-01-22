97745a58c5aafde5b129b32246c12108db102305
Itamar Junior <cdajuniorf@gmail.com>
2025-01-22 05:54:14 -0300

    feat: Iniciei a criação do Middleware

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,20 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
+use App\Http\Middleware\CheckIsLogged; 
 use Illuminate\Support\Facades\Route;
 
-// auth routes
+// Se o usuário não estiver logado, cairá em alguma das rotas abaixo
 Route::get('/login',  [AuthController::class, 'login']);
 Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
-Route::get('/logout', [AuthController::class, 'logout']);
+
+// Middleware para verificar se o usuário está logado
+Route::middleware([CheckIsLogged::class])->group(function () {
+
+    // Se o usuário estiver logado, cairá em uma das rotas abaixo
+    Route::get('/', [MainController::class, 'index']);
+    Route::get('/newNote', [MainController::class, 'newNote']);
+    Route::get('/logout', [AuthController::class, 'logout']);
+});
+
+

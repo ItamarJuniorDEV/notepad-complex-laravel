9860606916f44d395a8665fca10a38747e3d62e4
Itamar Junior <cdajuniorf@gmail.com>
2025-01-22 06:05:40 -0300

    feat: Criei um middleware adicional

--- app/Http/Middleware/CheckIsNotLogged.php
+++ app/Http/Middleware/CheckIsNotLogged.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class CheckIsLogged
+class CheckIsNotLogged
 {
     /**
      * Handle an incoming request.
@@ -15,9 +15,9 @@ class CheckIsLogged
      */
     public function handle(Request $request, Closure $next): Response
     {
-        // Verifica se o usuário está logado 
-        if (!session('user')) {
-            return redirect('/login');
+        // Verifica se o usuário não está logado 
+        if (session('user')) {
+            return redirect('/');
         }
         return $next($request);
     }
--- routes/web.php
+++ routes/web.php
@@ -3,15 +3,18 @@
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\MainController;
 use App\Http\Middleware\CheckIsLogged; 
+use App\Http\Middleware\CheckIsNotLogged;
 use Illuminate\Support\Facades\Route;
 
 // Se o usuário não estiver logado, cairá em alguma das rotas abaixo
-Route::get('/login',  [AuthController::class, 'login']);
-Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+Route::middleware([CheckIsNotLogged::class])->group(function () {
+  Route::get('/login',  [AuthController::class, 'login']);
+  Route::post('/loginSubmit', [AuthController::class, 'loginSubmit']);
+});
+
 
 // Middleware para verificar se o usuário está logado
 Route::middleware([CheckIsLogged::class])->group(function () {
-
     // Se o usuário estiver logado, cairá em uma das rotas abaixo
     Route::get('/', [MainController::class, 'index']);
     Route::get('/newNote', [MainController::class, 'newNote']);

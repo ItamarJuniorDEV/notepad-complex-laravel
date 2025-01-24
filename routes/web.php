0d62d797bf0fb2f1dadee538c43ddced4ded0920
Itamar Junior <cdajuniorf@gmail.com>
2025-01-24 04:57:08 -0300

    feat: Preparei a funcionalidade de edição de nota.

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,10 @@ Route::middleware([CheckIsLogged::class])->group(function () {
     Route::get('/', [MainController::class, 'index'])->name('home');
     Route::get('/newNote', [MainController::class, 'newNote'])->name('new');
     Route::post('/newNoteSubmit', [MainController::class, 'newNoteSubmit'])->name('newNoteSubmit');
+    
     // edit note
     Route::get('/editNote/{id}', [MainController::class, 'editNote'])->name('edit');  
+    Route::post('/editNoteSubmit', [MainController::class, 'editNoteSubmit'])->name('editNoteSubmit');  
 
     // delete note
     Route::get('/deleteNote/{id}', [MainController::class, 'deleteNote'])->name('delete');  

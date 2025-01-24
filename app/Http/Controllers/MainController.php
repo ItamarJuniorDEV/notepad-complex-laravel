256b1299afb0810fb129998be0c88f09467a6191
Itamar Junior <cdajuniorf@gmail.com>
2025-01-24 03:35:34 -0300

    feat: Validei o formulário de notes e criei uma nova rota.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\Services\Operations;
 use Illuminate\Contracts\Encryption\DecryptException;
@@ -29,7 +30,36 @@ class MainController extends Controller
 
     public function newNoteSubmit(Request $request)
     {
-        echo "I'm creating a new note";
+        // validate request
+        $request->validate(
+            // rules
+            [
+                'text_title' => 'required|min:3|max:200',
+                'text_note' => 'required|min:3 |max:3000'
+            ],
+            // error messages
+            [
+                'text_title.required' => 'O título é obrigatório',
+                'text_title.min' => 'O título deve ter no minimo :min caracteres',
+                'text_title.max' => 'O título deve ter no máximo :min caracteres',
+
+                'text_note.required' => 'A nota é obrigatória',
+                'text_note.min' => 'A nota deve ter no minimo 3 caracteres',
+                'text_note.max' => 'A anota deve ter no máximo 3000 caracteres'
+            ]
+        );
+        // get user id
+        $id = session('user.id');
+
+        // create new notes
+        $note = new Note();
+        $note->user_id = $id;
+        $note->title = $request->input('text_title');
+        $note->text = $request->input('text_note');
+        $note->save();
+
+        // redirect to home
+        return redirect()->route('home');
     }
 
     public function editNote($id)
